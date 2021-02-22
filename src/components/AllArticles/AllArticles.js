233e5c51148a59a663fd262d0184e2239cea9986
enrique-lol <enrique.memije.pro@gmail.com>
2021-02-21 21:48:07 -0600

    Article Content Article Onjects in components

---
+++ src/components/AllArticles/AllArticles.js
@@ -0,0 +1,23 @@
+// Initial Imports
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+
+// Define each individual article
+const articles = [
+  { articleId: 'first-article-ever',
+    'title': 'First Article Ever!',
+    'headline': 'Is this the first headline? Yes.',
+    'text': 'lorem impsum ONE lorem impsum ONE lorem impsum ONE lorem impsum ONE lorem impsum ONE lorem impsum ONE lorem impsum ONE' },
+
+  { articleId: 'worlds-second-article',
+    'title': 'Worlds Second Article!',
+    'headline': 'The return of Lorem Ipsum',
+    'text': 'lorem impsum TWO lorem impsum TWO lorem impsum TWO lorem impsum TWO lorem impsum TWO lorem impsum TWO lorem impsum TWO' },
+
+  { articleId: 'third-article-in-history',
+    'title': 'Third Article In History!',
+    'headline': 'Lorem Ipsum strikes back',
+    'text': 'lorem impsum THREE lorem impsum THREE lorem impsum THREE lorem impsum THREE lorem impsum THREE lorem impsum THREE lorem impsum THREE' }
+]
